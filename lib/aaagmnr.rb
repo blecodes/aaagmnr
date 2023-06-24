00d83cbb36f87259fb42441a18fbd9147c951ad7
Blessed Sibanda <blessedsibanda.dart@gmail.com>
2023-06-24 20:42:52 +0200

    Initialize gem

---
+++ lib/aaagmnr.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "aaagmnr/version"
+
+module Aaagmnr
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/aaagmnr_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Aaagmnr do
+  it "has a version number" do
+    expect(Aaagmnr::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
